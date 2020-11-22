3b29a12def6a22a6cea7f096cd9d2164f60d2917
Jason Zhang <jzhang20@stuy.edu>
2020-11-21 22:28:47 -0500

    getvalue, getdenominator, getnumerator

--- RationalNumber.java
+++ RationalNumber.java
@@ -3,5 +3,19 @@ public class RationalNumber extends RealNumber {
 
   public RationalNumber (int nume, int deno) {
     super((nume + 0.0) / deno);
+    numerator = nume;
+    denominator = deno;
+  }
+
+  public double getValue () {
+    return ((numerator + 0.0) / denominator);
+  }
+
+  public int getNumerator () {
+    return numerator;
+  }
+
+  public int getDenominator () {
+    return denominator;
   }
 }
